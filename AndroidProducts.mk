f0e6b958d061f514b9817dc3977a8c1c34fdd7d8
Dan Willemsen <dwillemsen@google.com>
2019-09-09 20:30:56 +0200

    cheeseburger: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Ibb9578ddfa43e241f868e2cf5a482a47482ee060
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosip.mk
+    $(LOCAL_DIR)/aosip_cheeseburger.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosip_cheeseburger-user \
+    aosip_cheeseburger-userdebug \
+    aosip_cheeseburger-eng
